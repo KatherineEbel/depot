f825f59999f2606379fbc5b18aaf2625382e3324
kathyebel <kathyebel@me.com>
2018-06-12 09:12:55 -0400

    Add cart to nav_sidebar.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,11 +32,14 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json do
+          render json:   @line_item.errors,
+                 status: :unprocessable_entity
+        end
       end
     end
   end
