fee3727a902afd97e12d00a5d06ab3fa75f1d122
kathyebel <kathyebel@me.com>
2018-06-12 15:44:41 -0400

    Add test to check remove cart with AJAX.

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -46,5 +46,18 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to store_index_url
+    assert_select '.cart aside', false
+  end
+
+  test 'should destroy cart via ajax' do
+    post line_items_url, params: { product_id: products(:ruby).id }
+    @cart = Cart.find(session[:cart_id])
+    assert_difference('Cart.count', -1) do
+      delete cart_url(@cart), xhr: true
+    end
+
+    assert_response :ok
+    assert_select '.cart aside', false
+
   end
 end
