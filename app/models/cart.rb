1cd5be64895abc2bb89da527bf796ebe268251b2
Katherine Ebel <kathyebel@me.com>
2018-06-11 22:25:47 -0400

    Implement showing quantity of line_items in cart.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,16 @@ class Cart < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
+
   private
 
   #ensure that there are no line items referencing this product
