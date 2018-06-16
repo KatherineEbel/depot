c0391c9059dc52e8ab368c90a77df1a3994d3d5e
Katherine Ebel <kathyebel@me.com>
2018-06-16 09:34:51 -0400

    Add authorization to admin functionality.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,9 +10,24 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 Rails.env = 'test'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  def login_as(user)
+    post login_path, params: { name: user.name, password: 'secret' }
+  end
+
+  def logout
+    delete logout_path
+  end
+
+  def setup
+    login_as users(:one)
+  end
 end
