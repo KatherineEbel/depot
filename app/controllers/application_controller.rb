0239d04bd458e3498ad400b31dcf1c9279b098aa
Katherine Ebel <kathyebel@me.com>
2018-06-17 15:32:33 -0400

    Add nav with admin links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: 'Please log in'
+    if request.format !Mime[:HTML]
+      authenticate_or_request_with_http_basic do |name, password|
+        user = User.find_by(name: name)
+        user&.authenticate(password) ? true : false
+      end
+    else
+      redirect_to login_url, notice: 'Please log in' unless User.find_by(id: session[:user_id])
     end
   end
 end
